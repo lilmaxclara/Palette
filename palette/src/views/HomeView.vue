7c22ca949964a499137b77a68fd224bc1f79e84e
ClaraMoon <Claralm2001@proton.me>
2022-10-20 23:54:20 +1100

    Add time

--- palette/src/views/HomeView.vue
+++ palette/src/views/HomeView.vue
@@ -9,26 +9,50 @@
       ></frameDisplay>
       <div v-if="!this.gameWonStatus">
         <searchBar @newSelectedItem="checkGuess($event)"></searchBar>
+      </div>
+      <div v-if="!this.gameWonStatus || this.showGuess">
         <guessDisplay :guessArray="this.guessArray"></guessDisplay>
       </div>
-      <div v-else>
+      <div v-if="this.gameWonStatus">
         <div class="bg-fuchsia-200 rounded-lg m-4 p-2">
           <div class="text-4xl text-green-500 pb-2">You won!</div>
+          <div class="text-3xl p-2">
+            <a :href="this.makeupURL">{{ this.gameWinningGuess }}</a>
+          </div>
           <div class="flex flex-row justify-center gap-2 py-2">
             <div
               v-for="guess in guessArray"
               :key="guess"
               class="p-4 rounded-lg bg-red-500"
-              :class="{'bg-green-500': guess === guessArray[guessArray.length - 1]}"
-            >
-            </div>
+              :class="{
+                'bg-green-500': guess === guessArray[guessArray.length - 1],
+              }"
+              
+            >Use a computed class here to switch between yes no and unknown guesses</div>
           </div>
           <div>
-            <button>Share</button>
+            <button
+              class="py-3 px-4 bg-green-500 rounded-lg text-white font-bold m-2"
+            >
+              SHARE
+            </button>
           </div>
-          <div class="text-3xl">
-            Buy the palette
-            <a :href="this.makeupURL">{{ this.gameWinningGuess }}</a>
+          <div>
+            <button
+              @click="this.showGuess = !this.showGuess"
+              class="underline m-2"
+            >
+              Show/Hide Guesses
+            </button>
+          </div>
+          <div class="flex flex-col justify-center p-2">
+            <div class="text-2xl">Next Palette:</div>
+            <VueCountdown class="font-bold text-xl"
+              :time=this.endOfDay() :transform="transformSlotProps" 
+              v-slot="{ hours, minutes, seconds }"
+            >
+              {{ hours }}:{{ minutes }}:{{ seconds }}
+            </VueCountdown>
           </div>
         </div>
       </div>
@@ -43,6 +67,8 @@ import imageDisplay from "@/components/imageDisplay";
 import frameDisplay from "@/components/frameDisplay";
 import searchBar from "@/components/searchBar";
 import guessDisplay from "@/components/guessDisplay";
+import VueCountdown from "@chenfengyuan/vue-countdown";
+
 const axios = require("axios").default;
 
 export default {
@@ -54,6 +80,7 @@ export default {
       currentImageURL: "",
       currentFrame: 1,
       makeupURL: "",
+      showGuess: false,
       guessArray: [],
       gameWinningGuess: "",
       gameWonStatus: false,
@@ -65,13 +92,14 @@ export default {
     frameDisplay,
     searchBar,
     guessDisplay,
+    VueCountdown,
   },
   async mounted() {
     //use axios to get the searchList and convert to json to array
 
     //calculate current game by taking a base date and adding the number of days since that date
     //this will be used to calculate the current game
-    const baseDate = new Date("2022-10-17");
+    const baseDate = new Date("2022-10-20");
     const currentDate = new Date();
     const diffTime = Math.abs(currentDate - baseDate);
     const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
@@ -123,9 +151,26 @@ export default {
         event +
         ".png";
     },
-    share(){
-      
-    }
+    share() {},
+    endOfDay() {
+      //calculate minutes hours and seconds before the end of the day
+    const currentDate = new Date();
+    const tomorrow = new Date(currentDate)
+    tomorrow.setDate(tomorrow.getDate() + 1)
+    tomorrow.setHours(0,0,0,0)
+    const diffTime = Math.abs(currentDate - tomorrow);
+    return diffTime
+    },
+    transformSlotProps(props) {
+      //Add leading zeros to the time
+      const formattedProps = {};
+
+      Object.entries(props).forEach(([key, value]) => {
+        formattedProps[key] = value < 10 ? `0${value}` : String(value);
+      });
+
+      return formattedProps;
+    },
   },
 };
 </script>
